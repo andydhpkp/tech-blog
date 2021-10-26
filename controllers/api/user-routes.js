aed1c141c93061f7fd3f9be0abb27aab766cde1f
Andrew Durham <andrewdurham1094@gmail.com>
2021-10-26 15:48:31 -0600

    finished pre-session User model and routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,4 +12,134 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        //include: [Post, Comment]
+    })
+    .then(dbUser => {
+        if (!dbUser) {
+            res.status(404).json({ message: 'No user found with this id' })
+            return
+        }
+        res.json(dbUser)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUser => {
+/*         req.session.save(() => {
+            req.session.user_id = dbUser.id;
+            req.session.username = dbUser.username;
+            req.session.loggedIn = true
+
+            res.json(dbUser)
+        }) */
+        res.json(dbUser)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+          username: req.body.username
+        }
+    }).then(dbUser => {
+        res.json({user: dbUser})
+    })
+})
+
+/* router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUser => {
+        if(!dbUser) {
+            res.status(400).json({ message: 'No user with that username found!' })
+            return
+        }
+
+        const validPassword = dbUser.checkPassword(req.body.password)
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' })
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUser.id
+            req.session.username = dbUser.username
+            req.session.loggedIn = true
+
+            res.json({ user: dbUser, message: 'You are now logged in!'})
+        })
+    })
+})
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end()
+        })
+    }
+    else {
+        res.status(404).end()
+    }
+}) */
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUser => {
+        if (!dbUser) {
+            res.status(404).json({ message: 'No user found with this id' })
+            return
+        }
+        res.json(dbUser)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUser => {
+        if (!dbUser) {
+            res.status(404).json({ message: 'No user found with this id' })
+            return
+        }
+        res.json(dbUser)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
 module.exports = router
